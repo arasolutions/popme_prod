723496e1c4f4465b17e41a41d239c491572f3fa1
spaukensen <antho33@gmail.com>
2018-12-26 23:43:48 +0100

    test

--- www/build/main.js
+++ www/build/main.js
@@ -6791,7 +6791,7 @@ var ProfilePage = /** @class */ (function () {
                             var options = {
                                 sourceType: 0,
                                 quality: 50,
-                                destinationType: _this.camera.DestinationType.FILE_URI,
+                                destinationType: _this.camera.DestinationType.DATA_URL,
                                 encodingType: _this.camera.EncodingType.JPEG,
                                 mediaType: _this.camera.MediaType.PICTURE,
                                 correctOrientation: true,
